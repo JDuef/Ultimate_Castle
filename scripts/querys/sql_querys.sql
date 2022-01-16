9443fc28cdf818390ea637a8a4e9dbb8e06a4790
Finn <contact@bechinka.eu>
2022-01-16 13:24:36 +0100

    Update sql_querys.sql

--- scripts/querys/sql_querys.sql
+++ scripts/querys/sql_querys.sql
@@ -4,26 +4,23 @@ Anforderung:
 Wenn sich ein Benutzer registriert soll dieser in Form eines neuen Datensatzes in die Account-Tabelle eingefügt werden.
 */
 /* Statement: */
+
 INSERT INTO ACCOUNT(acc_id, acc_username, acc_password, acc_email)
 VALUES ([Account ID], [Benutername], [Passwort], [E-Mail]);
+
 /* Beispielaufruf: */
 INSERT INTO ACCOUNT(acc_id, acc_username, acc_password, acc_email)
 VALUES (21, 'shaker', 'this is a test pw', 'shaker@web.de');
 
-/* account allianz und koenigreich zuweisen */
-/* NICHT MEHR GEBRAUCHT? */
+/* account allianz zuweisen */
 UPDATE ACCOUNT
-SET koe_id = [id eines Königreichs]
-WHERE acc_id = [id des accounts];
-
-UPDATE ACCOUNT
-SET al_id = [id einer allianz]
-WHERE acc_id = [id des accounts];
+SET al_id = [Allianz ID]
+WHERE acc_id = [Account ID];
 
 /* account loeschen */
 /* on delete cascade? */
 DELETE FROM ACCOUNT
-WHERE acc_id = [id des accounts];
+WHERE acc_id = [Account ID];
 
 DELETE FROM ACCOUNT
 WHERE acc_id = 21;
@@ -35,7 +32,7 @@ Wenn ein Spieler eine Auktion erstellt muss dies in der Auktion-Tabelle reflekti
 */
 /* Statement: */
 INSERT INTO AUKTION(auk_id, acc_id, item_id, auk_start_datum, auk_end_datum)
-VALUES ([ID des erstellenden Accounts], [ID des Items welches versteigert wird], [erstell Datum], [End-Datum der Auktion]);
+VALUES ([Account ID], [ID des Items welches versteigert wird], [erstell Datum], [End-Datum der Auktion]);
 /* Beispielaufruf: */
 INSERT INTO AUKTION(auk_id, acc_id, item_id, auk_start_datum, auk_end_datum)
 VALUES (6, 1, 1, TO_DATE('2022-01-12 13:34:26', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2022-01-13 13:34:26', 'yyyy/mm/dd hh24:mi:ss'));
@@ -60,13 +57,14 @@ Anforderung:
 Es soll ermöglicht werden alle abgelaufenden Auktionen zurückzugeben um diese zu Verabeiten. Also das höchste gebot zu ermitteln und das Item zu transferieren.
 */
 /* Statement: */
-CREATE VIEW Abgelaufende_Auktionen AS
+/* möchste gebot mit joinen*/
+CREATE VIEW Abgelaufene_Auktionen AS
 SELECT *
 FROM AUKTION
 WHERE CURRENT_TIMESTAMP >= auk_end_datum;
 /* Beispielaufruf: */
 SELECT *
-FROM Abgelaufende_Auktionen
+FROM Abgelaufene_Auktionen
 
 /* alle auktionen zurückgeben welche von einem bestimmten account erstellt wurden */
 /* VIELLEICHT NOCH JOIN MIT DEM HÖCHSTEN GEBOT MACHEN */
@@ -75,12 +73,16 @@ Anforderung:
 Alle auktionen sollen zurückgegeben werden welche von einem bestimmtem Account erstellt wurden um diese in einer "meine Auktionen" Übersicht anzuzeigen. 
 */
 /* Statement: */
-SELECT *
-FROM AUKTION
-WHERE acc_id = [id eines accounts]
+CREATE VIEW AuktionMitMaxGebot AS
+SELECT a.auk_id, a.acc_id, a.item_id, a.auk_start_datum, a.auk_end_datum, g.max_gebot
+FROM AUKTION a
+LEFT OUTER JOIN (
+    SELECT auk_id, max(gebot_wert) max_gebot
+    FROM GEBOT
+    GROUP BY auk_id) g ON g.auk_id = a.auk_id;
 
-SELECT *
-FROM AUKTION
+SELECT * 
+FROM AUKTIONMITMAXGEBOT
 WHERE acc_id = 1;
 
 /* gebot abgeben */
@@ -89,6 +91,8 @@ Anforderung:
 Wenn ein Spieler ein Gebot auf eine Auktion abgibt muss dieses in die Gebot-Tabelle eingefügt werden.
 */
 /* Statement: */
+INSERT INTO GEBOT(gebot_id, auk_id, acc_id, gebot_wert)
+VALUES([Gebot ID], [Auktion ID], [Account ID], [Gebot Wert]);
 /* Beispielaufruf: */
 INSERT INTO GEBOT(gebot_id, auk_id, acc_id, gebot_wert)
 VALUES(51, 1, 5, 3);
@@ -113,7 +117,7 @@ Anforderung:
 Das höchste gebot für eine bestimmte Auktion soll ermittelt werden um dieses in der GUI anzuzeigen oder zu ermitteln wer eine Auktion gewonnen hat nachdem diese abgelaufen ist.
 */
 /* Statement: */
-SELECT *
+SELECT gebot_wert
 FROM GEBOT
 WHERE auk_id = [id der Auktion] AND
 gebot_wert = (SELECT MAX(gebot_wert) FROM GEBOT WHERE auk_id = [id der Auktion])
@@ -127,17 +131,17 @@ gebot_wert = (SELECT MAX(gebot_wert) FROM GEBOT WHERE auk_id = 1);
 INSERT INTO BESTELLUNG(best_id, acc_id, best_datum, best_wert)
 VALUES ([id der Bestellung], [id eines Accounts], [Datum], [Bestellwert]);
 
-INSERT INTO bestellung_to_item(best_id, item_id, anzahl)
+INSERT INTO bestellung_to_item(best_id, item_id, BEST_ITEM_ANZAHL)
 VALUES ([id der Bestellung], [id eines Items], [Anzahl des Items]);
 /* WENN MEHRERE ITEMS DANN LETZTES INSERT WIEDERHOLEN*/
 
 INSERT INTO BESTELLUNG(best_id, acc_id, best_datum, best_wert)
 VALUES (21,1, CURRENT_TIMESTAMP, 42);
 
-INSERT INTO bestellung_to_item(best_id, item_id, anzahl)
+INSERT INTO bestellung_to_item(best_id, item_id, BEST_ITEM_ANZAHL)
 VALUES (21, 1, 3);
 
-INSERT INTO bestellung_to_item(best_id, item_id, anzahl)
+INSERT INTO bestellung_to_item(best_id, item_id, BEST_ITEM_ANZAHL)
 VALUES (21, 2, 1);
 
 /* alle items welche ein bestimmter account besitzt zurückgeben */
@@ -155,11 +159,11 @@ WHERE acc_id = 1;
 /* wenn der account schon ein item des typs besitzt */
 UPDATE ITEM_TO_ACCOUNT
 SET ANZAHL = ANZAHL + 1
-WHERE item_id = [id des items];
+WHERE item_id = [id des items] AMD acc_id = [ID eines Accounts];
 
 UPDATE ACCOUNT_TO_ITEM
 SET ANZAHL = ANZAHL + 1
-WHERE item_id = 11;
+WHERE item_id = 11 AND acc_id = 1;
 
 /* wenn der account noch kein item des typs besitzt */
 INSERT INTO ITEM_TO_ACCOUNT(acc_id, item_id, anzahl)
@@ -173,13 +177,13 @@ VALUES (1, 2, 1);
 
 /* item von account löschen */
 /* wenn der account mehr als ein item des typs besitzt */
-UPDATE ACCOUNT_TO_ITEM
-SET anzahl = anzhal - 1
-WHERE item_id = [id des items];
+UPDATE ITEM_TO_ACCOUNT
+SET ANZAHL = ANZAHL - 1
+WHERE item_id = [id des items] AMD acc_id = [ID eines Accounts];
 
 UPDATE ACCOUNT_TO_ITEM
 SET ANZAHL = ANZAHL - 1
-WHERE item_id = 11;
+WHERE item_id = 11 AND acc_id = 1;
 
 /* wenn der account nur noch ein item des typs besitzt */
 DELETE FROM ITEM_TO_ACCOUNT
@@ -187,3 +191,37 @@ WHERE item_id = [id des items] AND acc_id = [id des accounts];
 
 DELETE FROM ACCOUNT_TO_ITEM
 WHERE item_id = 2 AND acc_id = 1;
+
+
+/* AUKTION NACH ITEM ODER ATTRIBUT SUCHEN , MIN MAX WERT */
+SELECT * 
+FROM AUKTION
+INNER JOIN ITEM ON ITEM.item_id = AUKTION.item_id
+WHERE item_name LIKE (%[suchbegriff]%);
+
+
+SELECT * 
+FROM AUKTION
+INNER JOIN ITEM ON ITEM.item_id = AUKTION.item_id
+WHERE item_name LIKE (%booster%);
+
+
+CREATE VIEW AuktionSuche AS
+SELECT a.auk_id, a.item_id, i.item_name, a.auk_start_datum, a.auk_end_datum, g.max_gebot
+FROM AUKTION a
+INNER JOIN (
+    SELECT auk_id, max(gebot_wert) max_gebot
+    FROM GEBOT
+    GROUP BY auk_id) g ON g.auk_id = a.auk_id
+INNER JOIN ITEM i ON i.item_id = a.item_id
+WHERE a.auk_end_datum < CURRENT_TIMESTAMP;
+
+SELECT *
+FROM AuktionSuche
+WHERE item_name LIKE ('%[Suchbegriff]%')
+AND max_gebot < [Limit];
+
+SELECT *
+FROM AuktionSuche
+WHERE item_name LIKE ('%booster%')
+AND max_gebot < 25;
